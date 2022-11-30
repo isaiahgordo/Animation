72d8acaca1d39b48153057ba5116734fdbb008c5
Isaiah Gordon <GordIsai857@GoTVDSB.ca>
2022-11-30 08:42:20 -0500

    done

--- Game1.cs
+++ Game1.cs
@@ -12,12 +12,12 @@ namespace Animation
         Rectangle brownR, creamR, greyR, orangeR,whiteR,introR;
         Vector2 brownV, creamV,greyV,orangeV, mouseR;
         MouseCursor no,now;
-        MouseState mS;
-        Sceen sceen;
-        enum Sceen 
+        MouseState mS, mouseState;
+        Srceen srceen;
+        enum Srceen 
         { 
-            intro
-                
+            Intro,
+            TribleYard
         }
         public Game1()
         {
@@ -48,14 +48,14 @@ namespace Animation
             _graphics.PreferredBackBufferHeight = 500;
             introR = new Rectangle(0, 0, 800, 500);
             _graphics.ApplyChanges();
-
+            //people
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            //dads home
             // TODO: use this.Content to load your game content here
             brownT=Content.Load<Texture2D>("tribbleBrown");
             creamT = Content.Load<Texture2D>("tribbleCream");
@@ -72,43 +72,49 @@ namespace Animation
             mouseR = Mouse.GetState().Position.ToVector2();
             KeyboardState kstate=Keyboard.GetState();
             // TODO: Add your update logic here
-            if(sceen==Sceen.intro)
-                //ffffffffffffffffffffuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuck
-            if (kstate.IsKeyDown(Keys.A))
-            { whiteR.X = (int)mouseR.X;whiteR.Y = (int)mouseR.Y; }
-            if(greyR.Right>800||greyR.Left<0)
-                greyV.X*=-1;
-            greyR.X+=(int)greyV.X;
-            if(brownR.Top<0||brownR.Bottom>500)
-                brownV.Y*=-1;
-            brownR.Y += (int)brownV.Y;
-            if (creamR.Top < 0 || creamR.Bottom > 500)
-                creamV.Y *= -1;
-            else if (creamR.Right > 800 || creamR.Left <0)
-                creamV.X *= -1;
-            creamR.Y += (int)creamV.Y;
-            creamR.X += (int)creamV.X;
-            if(orangeR.Top<0||orangeR.Bottom > 500)
-                orangeV.Y *= -1;
-            else if(orangeR.Right>800||orangeR.Left <0)
-                orangeV.X *= -1;
-            orangeR.X += (int)orangeV.X;
-            orangeR.Y -= (int)orangeV.Y;
-            if(brownR.Contains(mouseR)&&no!=now)
+            mouseState=Mouse.GetState();
+            if (srceen == Srceen.Intro)
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    srceen = Srceen.TribleYard;
+                else;
+            else if (srceen == Srceen.TribleYard)
             {
-                now=MouseCursor.No;
-                brownR.Height += 10;
-                brownR.Width += 10;
+                if (kstate.IsKeyDown(Keys.A))
+                { whiteR.X = (int)mouseR.X; whiteR.Y = (int)mouseR.Y; }
+                if (greyR.Right > 800 || greyR.Left < 0)
+                    greyV.X *= -1;
+                greyR.X += (int)greyV.X;
+                if (brownR.Top < 0 || brownR.Bottom > 500)
+                    brownV.Y *= -1;
+                brownR.Y += (int)brownV.Y;
+                if (creamR.Top < 0 || creamR.Bottom > 500)
+                    creamV.Y *= -1;
+                else if (creamR.Right > 800 || creamR.Left < 0)
+                    creamV.X *= -1;
+                creamR.Y += (int)creamV.Y;
+                creamR.X += (int)creamV.X;
+                if (orangeR.Top < 0 || orangeR.Bottom > 500)
+                    orangeV.Y *= -1;
+                else if (orangeR.Right > 800 || orangeR.Left < 0)
+                    orangeV.X *= -1;
+                orangeR.X += (int)orangeV.X;
+                orangeR.Y -= (int)orangeV.Y;
+                if (brownR.Contains(mouseR) && no != now)
+                {
+                    now = MouseCursor.No;
+                    brownR.Height += 10;
+                    brownR.Width += 10;
+                }
+                else if (brownR.Contains(whiteR))
+                {
+                    brownR.Height += 10;
+                    brownR.Width += 10;
+                    whiteR.X = -10;
+                    whiteR.Y = -10;
+                }
+                if (brownR.Width > Window.ClientBounds.Width && brownR.Height > Window.ClientBounds.Height)
+                    base.Exit();
             }
-            else if(brownR.Contains(whiteR))
-            {
-                brownR.Height += 10;
-                brownR.Width += 10;
-                whiteR.X = -10;
-                whiteR.Y = -10;
-            }
-            if(brownR.Width>Window.ClientBounds.Width&&brownR.Height>Window.ClientBounds.Height)
-                base.Exit();
             base.Update(gameTime);
         }
 
@@ -118,9 +124,9 @@ namespace Animation
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            if (sceen == Sceen.intro)
+            if (srceen == Srceen.Intro)
                 _spriteBatch.Draw(introT, introR, Color.White);
-            else
+            else if(srceen==Srceen.TribleYard)
             { 
             _spriteBatch.Draw(brownT, brownR, Color.White);
             _spriteBatch.Draw(creamT, creamR, Color.White);
