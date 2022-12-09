0791eaa9ee47717c2ece0f906fe11fc8d31925b5
Isaiah Gordon <GordIsai857@GoTVDSB.ca>
2022-12-09 09:39:29 -0500

    worky woerrredfg

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Animation
 {
@@ -9,11 +10,11 @@ namespace Animation
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         Texture2D brownT, creamT, greyT, orangeT,whiteT,introT;
-        Rectangle brownR, creamR, greyR, orangeR,whiteR,introR;
-        Vector2 brownV, creamV,greyV,orangeV, mouseR;
-        MouseCursor no,now;
-        MouseState mS, mouseState;
+        Rectangle whiteR,introR;        
+        MouseState  mouseState;
         Srceen srceen;
+        List<Tribble> tribbes;
+        Tribble trible,tribble,tri,ble;
         enum Srceen 
         { 
             Intro,
@@ -28,28 +29,21 @@ namespace Animation
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            brownR = new Rectangle(300, 10, 100, 100);
-            creamR = brownR;
-            creamR.X = 10;
-            creamR.Y = 300;greyR = brownR;
-            orangeR = brownR;
-            greyR.X = 150;
-            greyR.Y = 150;
-            orangeR.X = 50;
-            orangeR.Y = 50;
-            brownV = new Vector2(0, 2);
-            creamV = new Vector2(2, 2);
-            greyV=new Vector2(2, 0);
-            orangeV=new Vector2(5, 3);
-            now = MouseCursor.Arrow;
-            no=MouseCursor.No;
+            // TODO: Add your initialization logic here            
             whiteR = new Rectangle(-10, -10, 50, 50);
             _graphics.PreferredBackBufferHeight = 500;
             introR = new Rectangle(0, 0, 800, 500);
             _graphics.ApplyChanges();
             //people
             base.Initialize();
+            trible = new Tribble(brownT, new Rectangle(300, 10, 100, 100), new Vector2(0, 2), Color.White);
+            tribble = new Tribble(creamT, new Rectangle(10, 300, 100, 100), new Vector2(2, 2), Color.White);
+            tri = new Tribble(greyT, new Rectangle(150, 150, 100, 100), new Vector2(2, 0), Color.White);
+            ble = new Tribble(orangeT, new Rectangle(50, 50, 100, 100), new Vector2(5, 3), Color.White);
+            tribbes = new List<Tribble> {trible,tribble,tri,ble };
+            tribbes.Add(new Tribble(tribble.texture,tribble.bounds,tribble.speed,Color.Pink));
+            tribbes.Add(new Tribble(tribble.texture, tribble.bounds, tribble.speed, Color.LightBlue));
+            tribbes.Add(new Tribble(tribble.texture, tribble.bounds, tribble.speed, Color.Yellow));
         }
 
         protected override void LoadContent()
@@ -62,77 +56,38 @@ namespace Animation
             greyT = Content.Load<Texture2D>("tribbleGrey");
             orangeT = Content.Load<Texture2D>("tribbleOrange");
             whiteT = Content.Load<Texture2D>("white");
-            introT = Content.Load<Texture2D>("introScreen");
+            introT = Content.Load<Texture2D>("intoScreen");
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            mouseR = Mouse.GetState().Position.ToVector2();
-            KeyboardState kstate=Keyboard.GetState();
+          
             // TODO: Add your update logic here
             mouseState=Mouse.GetState();
             if (srceen == Srceen.Intro)
                 if (mouseState.LeftButton == ButtonState.Pressed)
                     srceen = Srceen.TribleYard;
-                else;
-            else if (srceen == Srceen.TribleYard)
-            {
-                if (kstate.IsKeyDown(Keys.A))
-                { whiteR.X = (int)mouseR.X; whiteR.Y = (int)mouseR.Y; }
-                if (greyR.Right > 800 || greyR.Left < 0)
-                    greyV.X *= -1;
-                greyR.X += (int)greyV.X;
-                if (brownR.Top < 0 || brownR.Bottom > 500)
-                    brownV.Y *= -1;
-                brownR.Y += (int)brownV.Y;
-                if (creamR.Top < 0 || creamR.Bottom > 500)
-                    creamV.Y *= -1;
-                else if (creamR.Right > 800 || creamR.Left < 0)
-                    creamV.X *= -1;
-                creamR.Y += (int)creamV.Y;
-                creamR.X += (int)creamV.X;
-                if (orangeR.Top < 0 || orangeR.Bottom > 500)
-                    orangeV.Y *= -1;
-                else if (orangeR.Right > 800 || orangeR.Left < 0)
-                    orangeV.X *= -1;
-                orangeR.X += (int)orangeV.X;
-                orangeR.Y -= (int)orangeV.Y;
-                if (brownR.Contains(mouseR) && no != now)
-                {
-                    now = MouseCursor.No;
-                    brownR.Height += 10;
-                    brownR.Width += 10;
-                }
-                else if (brownR.Contains(whiteR))
-                {
-                    brownR.Height += 10;
-                    brownR.Width += 10;
-                    whiteR.X = -10;
-                    whiteR.Y = -10;
-                }
-                if (brownR.Width > Window.ClientBounds.Width && brownR.Height > Window.ClientBounds.Height)
-                    base.Exit();
-            }
+            if (srceen == Srceen.TribleYard)
+                foreach (var tibble in tribbes)
+                    tibble.Move(_graphics);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
             if (srceen == Srceen.Intro)
                 _spriteBatch.Draw(introT, introR, Color.White);
             else if(srceen==Srceen.TribleYard)
-            { 
-            _spriteBatch.Draw(brownT, brownR, Color.White);
-            _spriteBatch.Draw(creamT, creamR, Color.White);
-            _spriteBatch.Draw(greyT, greyR, Color.White);
-            _spriteBatch.Draw(orangeT, orangeR, Color.White);
-            _spriteBatch.Draw(whiteT, whiteR, Color.White); }
+            {
+              foreach(var tibble in tribbes)  
+                tibble.Draw(_spriteBatch);
+             _spriteBatch.Draw(whiteT, whiteR, Color.White);
+            }
             _spriteBatch.End();
             base.Draw(gameTime);
         }
---
+++ Tribble.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Color = Microsoft.Xna.Framework.Color;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+
+namespace Animation
+{
+    class Tribble
+    {
+        private Texture2D _texture;
+        private Rectangle _bounds;
+        private Color _colour;
+        private Vector2 _speed;
+        public Tribble(Texture2D texture, Rectangle bounds, Vector2 speed, Color colour)
+        {
+            _texture = texture;
+            _bounds = bounds;
+            _colour = colour;
+            _speed = speed;
+
+        }
+
+        public Texture2D texture
+        {
+            get
+            {
+                return _texture;
+            }
+        }
+        public Rectangle bounds
+        {
+            get { return _bounds; }
+            set { _bounds = value; }
+        }
+        public Color colour
+        {
+            get { return _colour; }
+            set { _colour = value; }
+        }
+        public Vector2 speed
+        {
+            get { return _speed;}
+        }
+        public void Move(GraphicsDeviceManager graph)
+        {
+            _bounds.Offset(_speed);
+            if (_bounds.Right > graph.PreferredBackBufferWidth || _bounds.Left < 0)
+                _speed.X *= -1;
+            _bounds.X += (int)_speed.X;
+            if (_bounds.Bottom > graph.PreferredBackBufferHeight || _bounds.Top < 0)
+                _speed.Y *= -1;
+            _bounds.Y += (int)_speed.Y;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _bounds, _colour);
+        }
+    }
+
+}
